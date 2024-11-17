720c23beabff57361ed6ee836b3e9d8fd5954519
wladislas.gorka <wladislas.gorka@gmail.com>
2024-11-17 22:48:55 +0100

    MyPlan avec MyForm

--- src/components/MyPlan.js
+++ src/components/MyPlan.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
 import Home from './Home';
+import MyForm from './MyForm';
 
 const MyPlan = ({plans}) => {
   const [selectedPlan, setSelectedPlan] = useState(null);
+  const fields = [
+    { name: 'nom', label: 'Nom', type: 'text', placeholder: 'Entrer votre Nom' },
+    { name: 'email', label: 'Email', type: 'email', placeholder: 'Entrer votre email' },
+  ];
 
   const handleSelectPlan = (planName) => {
     setSelectedPlan(planName);
@@ -20,7 +25,14 @@ const MyPlan = ({plans}) => {
     <div className="container mx-auto">
       <Home plans={plans} onSelectPlan={handleSelectPlan} />
       {selectedPlan && (
-        <form onSubmit={handleSubmit} className="mt-4">
+        <>
+        <div className="bg-gray-900 py-16">
+        <div className="container mx-auto p-4">
+          <h2 className="text-2xl font-bold mb-4">Inscription à la formule {selectedPlan}</h2>
+          <MyForm fields={fields} onSubmit={handleSubmit} />
+        </div>
+        </div>
+        {/* <form onSubmit={handleSubmit} className="mt-4">
           <h2 className="text-xl font-bold mb-2">Inscription à la formule {selectedPlan}</h2>
           <div className="mb-4">
             <label htmlFor="name" className="block mb-2 font-bold">Nom</label>
@@ -45,7 +57,8 @@ const MyPlan = ({plans}) => {
           <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded">
             S'inscrire
           </button>
-        </form>
+        </form> */}
+        </>
       )}
     </div>
   );
