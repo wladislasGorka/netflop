1191cc8c54638a7e8f7bca42bb10a23f669a2f95
wladislas.gorka <wladislas.gorka@gmail.com>
2024-11-19 16:59:05 +0100

    animation

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,19 +1,27 @@
 import React from "react";
+import { CSSTransition } from 'react-transition-group';
 import '../Modal.css';
 
 const Modal = ({show, onClose}) => {
-    if (!show) {
-        return null;
-    }
+    // if (!show) {
+    //     return null;
+    // }
     
     return (
-    <div className="modal-overlay">
-        <div className="modal-content">
-        <button className="modal-close" onClick={onClose}>X</button>
-        <h2>Contenu de la modale</h2>
-        <p>Ceci est une fenêtre modale simple.</p>
+        <CSSTransition
+        in={show}
+        timeout={300}
+        classNames="modal"
+        unmountOnExit
+        >
+        <div className="modal-overlay" onClick={onClose}>
+          <div className="modal-content" onClick={e => e.stopPropagation()}>
+            <button className="modal-close" onClick={onClose}>X</button>
+            <h2>Contenu de la modale</h2>
+            <p>Ceci est une fenêtre modale avec animation.</p>
+          </div>
         </div>
-    </div>
+      </CSSTransition>
     );
 
 }
