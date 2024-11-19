897dc79b9a505d0e3e95623cf6caa019b892d140
wladislas.gorka <wladislas.gorka@gmail.com>
2024-11-19 16:17:54 +0100

    tentative emailjs

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,17 @@ import React from 'react';
 
 function About (){
     return (
-        <div>
-            <h3>A propos</h3>
+        <div className="bg-gray-900 p-16">
+            <h1 className='text-2xl font-bold'>A propos</h1>
+            <h2 className="mb-8 text-xl text-gray-400 leading-relaxed text-justify">
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+            </h2>
+            <h2 className="mb-8 text-xl text-gray-400 leading-relaxed text-justify">
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+            </h2>
+            <h2 className="mb-8 text-xl text-gray-400 leading-relaxed text-justify">
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+            </h2>
         </div>
     )
 };
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MyForm from './MyForm';
+import emailjs from '@emailjs/browser';
 
 function Contact(){
     const fields = [ 
@@ -8,8 +9,26 @@ function Contact(){
         { name: 'message', label: 'Message', type: 'textarea', placeholder: 'Ecrire votre message' }, ]; 
         
         const handleSubmit = (formData) => {
-             console.log('Form Data:', formData); 
-            }; 
+             console.log('Form Data:', formData);
+
+             emailjs.init({
+                publicKey: "DpYHXZu_9v4J3Tj1l",
+              }); 
+
+             emailjs
+             .sendForm('service_1m3vpfn', 'template_ay1dm7k', formData, {
+               publicKey: 'DpYHXZu_9v4J3Tj1l',
+             })
+             .then(
+               () => {
+                 console.log('SUCCESS!');
+               },
+               (error) => {
+                 console.log('FAILED...', error.text);
+               },
+             );
+        }; 
+
         return ( 
             <div className="bg-gray-900 py-16">
             <div className="container mx-auto px-4"></div>
